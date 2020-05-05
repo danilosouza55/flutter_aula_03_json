3fb192f6c9231ee95cccb5194dfeb22781ca87b7
Danilo Araujo de Souza <danilo.souza@edu.unipar.br>
2020-05-05 19:53:16 -0300

    Adicionado na Home_Page a busca dos dados

---
+++ lib/ui/detalhes_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DetalhesPage extends StatefulWidget {
+  @override
+  _DetalhesPageState createState() => _DetalhesPageState();
+}
+
+class _DetalhesPageState extends State<DetalhesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:transparent_image/transparent_image.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -27,9 +28,88 @@ class _HomePageState extends State<HomePage> {
           "Emissoras",
           style: TextStyle(color: Colors.white, fontSize: 24),
         ),
+        centerTitle: true,
       ),
       backgroundColor: Color(0xFFEEEEEE),
-      body: Column(),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: FutureBuilder(
+              future: _getLista(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Center(
+                      child: Container(
+                        width: 200,
+                        height: 200,
+                        alignment: Alignment.center,
+                        child: CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.black),
+                          strokeWidth: 5,
+                        ),
+                      ),
+                    );
+                  default:
+                    if (snapshot.hasError)
+                      return Container(
+                        child: Text(
+                          "Erro ao acessar a internet",
+                          style: TextStyle(color: Colors.amber[900]),
+                        ),
+                      );
+                    else
+                      return criarListagem(context, snapshot);
+                }
+              },
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  Widget criarListagem(BuildContext context, AsyncSnapshot snapshot) {
+    return ListView.builder(
+        padding: EdgeInsets.all(4),
+        scrollDirection: Axis.vertical,
+        itemCount: snapshot.data.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            child: Card(
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: Row(
+                  children: <Widget>[
+                    FadeInImage.memoryNetwork(
+                      placeholder: kTransparentImage,
+                      image: snapshot.data[index]["Emi_Logo"]
+                          .toString()
+                          .replaceAll(
+                              "~/", "http://controle.mdvsistemas.com.br/"),
+                      height: 75,
+                      width: 75,
+                      fit: BoxFit.cover,
+                    ),
+                    Expanded(
+                      child: Padding(
+                          padding: EdgeInsets.only(left: 10, right: 10),
+                          child: Text(
+                            snapshot.data[index]["Emi_Nome"].toString(),
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold),
+                          )),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            onTap: () {},
+          );
+        });
   }
 }
